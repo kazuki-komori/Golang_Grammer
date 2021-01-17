d21e4c09347121f33cdf7fd6e25d90dc2ea72512
kazuki-komori <komokazu.nankai@gmail.com>
2021-01-17 19:02:48 +0900

    add: router example

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "go_sample/arguments"
+import (
+	"fmt"
+	"go_sample/src/github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
-	arguments.ReceiverPrinter()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
