c9eaaa69d5962ca45f3502f04c05ec7c136ab1b0
kazuki-komori <komokazu.nankai@gmail.com>
2021-01-18 00:42:38 +0900

    add: api sample

--- example/rest.go
+++ example/rest.go
@@ -1,1 +1,39 @@
 package example
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func Router() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", home)
+	router.HandleFunc("/users", findAllUsers)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+}
+
+func findAllUsers(w http.ResponseWriter, r *http.Request) {
+	var userList = []User{
+		{ID: 1, FirstName: "山田", LastName: "太郎"},
+		{ID: 2, FirstName: "鈴木", LastName: "一郎"},
+	}
+
+	response, _ := json.Marshal(userList)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"go_sample/src/github.com/gorilla/mux"
-	"log"
-	"net/http"
-)
+import "go_sample/example"
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	example.Router()
 }
