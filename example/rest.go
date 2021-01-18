ca615567566500b6d8222764a6763790a05398de
kazuki-komori <komokazu.nankai@gmail.com>
2021-01-19 00:06:50 +0900

    add: gorm api example

--- example/rest.go
+++ example/rest.go
@@ -4,8 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func Router() {
@@ -26,14 +31,38 @@ type User struct {
 }
 
 func findAllUsers(w http.ResponseWriter, r *http.Request) {
-	var userList = []User{
-		{ID: 1, FirstName: "山田", LastName: "太郎"},
-		{ID: 2, FirstName: "鈴木", LastName: "一郎"},
-	}
+	db := connectToDB()
+	defer db.Close()
+
+	db.LogMode(true)
+
+	var userList []User
+
+	db.Find(&userList)
+	fmt.Println(userList)
+	fmt.Println(&userList)
 
 	response, _ := json.Marshal(userList)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
+}
+
+func connectToDB() *gorm.DB {
+	err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV")))
+	env := os.Getenv("ENV")
+	DB := os.Getenv("DB")
+	DBUser := os.Getenv("DB_USER")
+	DBPass := os.Getenv("DB_PASS")
+
+	CONNECT := DBUser + ":" + DBPass + "@" + env + "/" + DB
+	fmt.Println(CONNECT)
+
+	db, err := gorm.Open("mysql", CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/stretchr/testify v1.7.0
 )
